0bcd6e4425868bc0e5546c5b55b963d316a82017
Metacraft <rafy.vec@gmail.com>
2024-03-15 15:30:53 +0100

    Tout

--- Scenes/main_character.gd
+++ Scenes/main_character.gd
@@ -8,7 +8,7 @@ const JUMP_VELOCITY = -900.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var double = 0
 
 func _physics_process(delta):
 	#Animations
@@ -21,7 +21,14 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		sprite_2d.animation = "jump"
-		
+
+		if Input.is_action_just_pressed("jump") and double == 0 :
+			velocity.y = JUMP_VELOCITY
+			double = 1
+	
+	if is_on_floor() :
+		double = 0
+
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -12,6 +12,6 @@ func _on_level_2_pressed():
 func _process(_delta) :
 	if Input.is_action_just_pressed("jump") :
 		get_tree().change_scene_to_file("res://Scenes/level1.tscn")
-	if Input.is_action_just_pressed("crouch") :
+	if Input.is_action_just_pressed("roll") :
 		get_tree().change_scene_to_file("res://Scenes/level2.tscn")
 	pass

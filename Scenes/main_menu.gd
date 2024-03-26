ee19fca71f22448dfbe1fef49a986d87a1022a49
Metacraft <rafy.vec@gmail.com>
2024-03-26 11:21:35 +0100

    level3

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -8,6 +8,8 @@ func _on_level_1_pressed():
 func _on_level_2_pressed():
 	get_tree().change_scene_to_file("res://Scenes/level2.tscn")
 
+func _on_level_3_pressed() :
+	get_tree().change_scene_to_file("res://Scenes/level3.tscn")
 
 func _process(_delta) :
 	if Input.is_action_just_pressed("jump") :

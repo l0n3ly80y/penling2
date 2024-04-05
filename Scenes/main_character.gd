8ee9c0b60fe6775daa432992a3a77f604f160867
Metacraft <rafy.vec@gmail.com>
2024-04-05 14:44:10 +0200

    Death animation

--- Scenes/main_character.gd
+++ Scenes/main_character.gd
@@ -9,39 +9,55 @@ const JUMP_VELOCITY = -900.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var double = 0
+var isHit = false
+
+func start_death():
+	isHit = true
+	sprite_2d.play("death")
 
 func _physics_process(delta):
 	#Animations
-	if (velocity.x > 1 || velocity.x < -1) :
-		sprite_2d.animation = "walk"
-	else :
-		sprite_2d.animation = "default"
-	
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-		sprite_2d.animation = "jump"
+	if not isHit :
+		if (velocity.x > 1 || velocity.x < -1) :
+			sprite_2d.animation = "walk"
+		else :
+			sprite_2d.animation = "default"
+		
+		# Add the gravity.
+		if not is_on_floor():
+			velocity.y += gravity * delta
+			sprite_2d.animation = "jump"
 
-		if Input.is_action_just_pressed("jump") and double == 0 :
+			if Input.is_action_just_pressed("jump") and double == 0 :
+				velocity.y = JUMP_VELOCITY
+				double = 1
+		
+		if is_on_floor() :
+			double = 0
+
+		# Handle jump.
+		if Input.is_action_just_pressed("jump") and is_on_floor():
 			velocity.y = JUMP_VELOCITY
-			double = 1
+
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction = Input.get_axis("left", "right")
+		if direction :
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, 2000*delta)
+
+		move_and_slide()
+
+		var isLeft = velocity.x < 0
+		sprite_2d.flip_h = isLeft
 	
-	if is_on_floor() :
-		double = 0
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, 2000*delta)
-
-	move_and_slide()
-
-	var isLeft = velocity.x < 0
-	sprite_2d.flip_h = isLeft
+	
+	else :
+		if sprite_2d.frame == 6 :
+			sprite_2d.play("desappearing")
+		if sprite_2d.animation == "desappearing" and sprite_2d.frame == 6 :
+			isHit = false
+			sprite_2d.animation = "default"
+			position.x = 837
+			position.y = 1564
--- Scenes/saw.gd
+++ Scenes/saw.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+@onready var game_manager = %GameManager
+@onready var character_body_2d = $"../SceneObjects/CharacterBody2D"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,4 +13,5 @@ func _process(_delta):
 	pass
 
 func _on_body_entered(body):
-	pass
+	if (body.name == "CharacterBody2D") :
+		character_body_2d.start_death()

57f927b6902ead5d6f96d1086b5596954bf7156f
Metacraft <rafy.vec@gmail.com>
2024-04-23 10:04:23 +0200

    Tweaking

--- Scenes/game_manager.gd
+++ Scenes/game_manager.gd
@@ -3,9 +3,11 @@ extends Node
 @onready var points_label = %PointsLabel
 
 var points = 0
-var current_scene = null
 
 func add_point() :
+	"""
+	Ajoute un point au joueur et modifie le texte du label
+	"""
 	points += 1
 	print(points)
 	points_label.text = "Points : " + str(points)
--- Scenes/main_character.gd
+++ Scenes/main_character.gd
@@ -4,9 +4,10 @@ extends CharacterBody2D
 const SPEED = 325.0
 const JUMP_VELOCITY = -900.0
 
-@onready var sprite_2d = $Sprite2D
+#Variables
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+@onready var playerSprite = $AnimatedSprite2D
+
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var doubleJumping = 0
 var isHit = false
@@ -14,29 +15,38 @@ var isSpawning = false
 var spawnCoordinates = Vector2(829,1592)
 
 func start_death():
+	"""
+	Démarre l'annimation de mort
+	"""
 	isHit = true
-	sprite_2d.play("hit")
+	playerSprite.play("hit")
 
 func spawn():
+	"""
+	Luciens remplis ça stp
+	"""
 	isSpawning = true
 	position = spawnCoordinates
-	sprite_2d.play("appearing")
+	playerSprite.play("appearing")
 
 func _ready() :
+	"""
+	Démarre l'animation de spawn au lancement du jeu
+	"""
 	spawn()
 
 func _physics_process(delta):
 	#Animations
 	if not isHit and not isSpawning :
 		if (velocity.x > 1 || velocity.x < -1) :
-			sprite_2d.animation = "walk"
+			playerSprite.animation = "walk"
 		else :
-			sprite_2d.animation = "idle"
+			playerSprite.animation = "idle"
 			
-		# Add the gravity.
+		#Gravité et sauts
 		if not is_on_floor():
 			velocity.y += gravity * delta
-			sprite_2d.animation = "jump"
+			playerSprite.animation = "jump"
 
 			if Input.is_action_just_pressed("jump") and doubleJumping == 0 :
 				velocity.y = JUMP_VELOCITY
@@ -49,8 +59,7 @@ func _physics_process(delta):
 		if Input.is_action_just_pressed("jump") and is_on_floor():
 			velocity.y = JUMP_VELOCITY
 
-		# Get the input direction and handle the movement/deceleration.
-		# As good practice, you should replace UI actions with custom gameplay actions.
+		#Déplacements latéraux
 		var direction = Input.get_axis("left", "right")
 		if direction :
 			velocity.x = direction * SPEED
@@ -59,21 +68,24 @@ func _physics_process(delta):
 
 		move_and_slide()
 
+		#Orientation du sprite
 		if (velocity.x < -1):
-			sprite_2d.flip_h =  true 
+			playerSprite.flip_h =  true 
 		if (velocity.x > 1):
-			sprite_2d.flip_h = false 
+			playerSprite.flip_h = false 
 
 	
 	else :
 		if isHit :
-			if sprite_2d.frame == 6 :
-				sprite_2d.play("desappearing")
-			if sprite_2d.animation == "desappearing" and sprite_2d.frame == 6 :
+			#Fait disparaitre et réapparaite le joueur
+			if playerSprite.frame == 6 :
+				playerSprite.play("desappearing")
+			if playerSprite.animation == "desappearing" and playerSprite.frame == 6 :
 				isHit = false
 				spawn()
 		if isSpawning :
-			if sprite_2d.frame == 6 :
+			#Immobilise le joueur le temps se son apparition
+			if playerSprite.frame == 6 :
 				velocity = Vector2(0,0)
 				isSpawning = false
 

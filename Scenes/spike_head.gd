9e6bb784956f71c2f026eebbd623c347d4246e79
Metacraft <rafy.vec@gmail.com>
2024-05-08 00:07:03 +0200

    Update spike_head.gd

--- Scenes/spike_head.gd
+++ Scenes/spike_head.gd
@@ -6,13 +6,14 @@ extends Node2D
 @onready var player = $"../CharacterBody2D"
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var ground_check = $ground_check
+@onready var starting_pos = position
 
 var y_entered = false
 var x_entered = false
 var is_activated = false
 var has_collided = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
 	if is_activated and not has_collided :
 		if y_entered == true :
@@ -22,13 +23,24 @@ func _process(delta):
 				position.y -= 500*delta
 		if x_entered == true :
 			if player.position.x >= position.x :
-				position.x -= 500*delta
+				position.x += 500*delta
 			else :
 				position.x -= 500*delta
+		if position == starting_pos :
+			is_activated = false
+	elif has_collided :
+		if position == starting_pos :
+			has_collided = false
+			x_entered = false
+			y_entered = false
+			is_activated = false
+		else :
+			position = position.move_toward(starting_pos, 250*delta)
 
 
 func _on_main_body_body_entered(body):
 	if (body.name == "CharacterBody2D") :
+		has_collided = true
 		player.start_death()
 
 func _on_detect_zone_y_body_entered(body):
@@ -50,7 +62,3 @@ func _on_ground_check_body_entered(body):
 		has_collided = true
 		x_entered = false
 		y_entered = false
-		#animated_sprite_2d.play("blink")
-		#if animated_sprite_2d.animation == "blink" and animated_sprite_2d.frame == 3 :
-		position = Vector2(1836,1197)
-		has_collided = false
